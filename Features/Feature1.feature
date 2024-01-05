7e01049d8c52d417fa2bab7aae4fdcd763b74cd5
NikhilaVecham <NikhilaVecham@PQA-0598LT>
2024-01-05 13:10:57 -0600

    Add project files.

---
+++ Features/Feature1.feature
@@ -0,0 +1,13 @@
+﻿Feature: Youtube search feature
+
+Search for the Testers Talk
+
+@TestersTalk
+Scenario: Search for TestersTalk
+	Given Open the browser
+	When Enter the URL
+	Then Search for the Testers Talk
+Scenario: Search for TestersTalk2
+	Given Open the browser
+	When Enter the URL
+	Then Search for the Testers Talk
---
+++ StepDefinitions/Feature1StepDefinitions.cs
@@ -0,0 +1,43 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SpecFlowProject1.StepDefinitions
+{
+    [Binding]
+    public sealed class Feature1StepDefinitions
+    {
+        private IWebDriver driver;
+
+        public Feature1StepDefinitions(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        [Given(@"Open the browser")]
+        public void GivenOpenTheBrowser()
+        {
+            //driver = new ChromeDriver();
+            //driver.Manage().Window.Maximize();
+        }
+
+        [When(@"Enter the URL")]
+        public void WhenEnterTheURL()
+        {
+            driver.Url = "https://www.youtube.com/";
+            Thread.Sleep(5000);
+        }
+
+        [Then(@"Search for the Testers Talk")]
+        public void ThenSearchForTheTestersTalk()
+        {
+            driver.FindElement(By.XPath("//*[@name='earch_query']")).SendKeys("Testers Talk");
+            driver.FindElement(By.XPath("//*[@name='search_query']")).SendKeys(Keys.Enter);
+
+            Thread.Sleep(5000);
+
+            //driver.Quit();
+        }
+
+
+    }
+}
